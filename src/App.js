e0e09e283d682f92badadf29b024adf89113026b
gramscott <120787031+gramscott@users.noreply.github.com>
2024-02-09 17:24:49 +0000

    logo added and in the center

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import NavBar from './NavBar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import './NavBar.css'
+import logo from '../images/JMD Alloys.jpeg'
 
 const NavBar = () => {
 
   return (
     
+    <nav>
     <div>
-    picture services get a quote Meet Josh FAQ Terms and conditions
+    <li className='text-center'>
+      <img src = {logo} alt = 'logo'/>
+      </li>
+
+ <ul>
+      <li>Services</li>
+      <li>Picture</li>
+      <li>Contact</li>
+      <li>Meet Josh</li>
+      <li>FAQ</li>
+      <li>Terms and conditions</li>
+    </ul>
     </div>
+    </nav>
 
   )
 }

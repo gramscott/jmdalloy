7702ac286d634450062cbcf82673039c57c7043a
gramscott <120787031+gramscott@users.noreply.github.com>
2024-02-11 17:54:23 +0000

    navbar completed

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,40 +1,36 @@
 import React from 'react';
-import './NavBar.css'; 
+import './NavBar.css';
 import logo from '../images/JMD Alloys.jpeg';
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid flex-column align-items-center align-items-lg-start">
-        <a href="/" className="navbar-brand mb-3 mb-lg-0">
-          <img src={logo} alt="logo" className="logo"/>
+    <nav className="navbar-expand-lg">
+      <div className="logo-container">
+        <a href="/" className="navbar-brand">
+          <img src={logo} alt="logo" className="logo active"/>
         </a>
-
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav ">
-            <li className="nav-item">
-              <a to="/services" className="nav-link">Services</a>
-            </li>
-            <li className="nav-item">
-              <a to="/contact" className="nav-link">Contact</a>
-            </li>
-            <li className="nav-item">
-              <a to="/meet-josh" className="nav-link">Meet Josh</a>
-            </li>
-            <li className="nav-item">
-              <a to="/faq" className="nav-link">FAQ</a>
-            </li>
-            <li className="nav-item">
-              <a to="/terms-and-conditions" className="nav-link">Terms and conditions</a>
-            </li>
-          </ul>
-        </div>
       </div>
-    </nav>
+
+      <ul className="nav">
+      <li className="nav-item">
+      <a className="nav-link active" href="/services">Services</a>
+      </li>
+      <li className="nav-item">
+        <a className='nav-link active' href='/get-a-quote'>Get a Quote</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" href="/about-josh">About Josh</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" href="/faq">Frequently Asked Questions</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" href="/terms-conditions">Terms & Conditions</a>
+        </li>
+      </ul>
+
+      </nav>
+
   );
 };
 

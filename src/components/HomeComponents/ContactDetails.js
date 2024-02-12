252c2441506e748eef862c7ed4982ac273b9df87
gramscott <120787031+gramscott@users.noreply.github.com>
2024-02-12 15:15:06 +0000

    implemented the address. Now onto creating the table

--- src/components/HomeComponents/ContactDetails.js
+++ src/components/HomeComponents/ContactDetails.js
@@ -4,6 +4,8 @@ import { Telephone, Envelope } from 'react-bootstrap-icons'
 
 const ContactDetails = () => {
   return (
+    <div>
+
     <div className='contact-wrapper'>
 
     <div className='contacts'>
@@ -16,7 +18,34 @@ const ContactDetails = () => {
     jmdalloyrepairs@gmail.com
     </b>
      </div>
+     </div>
+
+     <div className='address-wrapper'>
+    <ul className='address'>
+     <li>
+     178 hyndford road,
+     </li>
+    <li>
+    ML11 9BH,
+    </li>
+    <li>
+    Lanark
+    </li>
+     </ul>
     </div>
+    <div>
+    <table className='table'>
+    <h3>
+    Opening Hours
+    </h3>
+    </table>
+    </div>
+    </div>
+
+
+
+    
+
   )
 }
 

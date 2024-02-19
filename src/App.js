84ec890dc3979824a2ebbfec8ac2673a192913cc
gramscott <120787031+gramscott@users.noreply.github.com>
2024-02-19 20:04:59 +0000

    my nav links work

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
      <NavBar/>
      <Routes>
-     <Route path="" element={HomeContainer}/>
+     <Route path="" element={<HomeContainer/>}/>
      <Route path="/services" element={<ServicesContainer/>}/>
      </Routes>
     </BrowserRouter>
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import React, { useState} from 'react';
 import './NavBar.css';
 import logo from '../images/JMD_Alloys_logo.jpg';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
 
@@ -21,11 +22,13 @@ const NavBar = () => {
       
       <div className={`nav-menu ${isNavExpanded ? 'nav-menu-expanded' : ''}`}>
       <ul className="nav justify-content-center">
+
+
       <li className="nav-item">
-      <a href="/" className="nav-link active" href="/">Home</a>
+      <Link to="/" className="nav-link active">Home</Link>
       </li>
       <li className="nav-item">
-      <a className="nav-link active" href="/services">Services</a>
+      <Link to="/services"className="nav-link active" >Services</Link>
       </li>
       <li className="nav-item">
         <a className='nav-link active' href='/get-a-quote'>Get a Quote</a>

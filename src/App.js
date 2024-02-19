2beee92a2bfe5297999d1bd52cf7b1156a2d4375
gramscott <120787031+gramscott@users.noreply.github.com>
2024-02-19 20:00:22 +0000

    i can display the service component but my home has disappeared

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import HomeContainer from './containers/HomeContainer';
 import NavBar from './NavBar/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Switch, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,  Routes, Route } from 'react-router-dom';
 import ServicesContainer from './containers/ServicesContainer';
 
 
 function App() {
   return (
     <div className="App">
+    <BrowserRouter>
      <NavBar/>
-     <HomeContainer/>
-      <ServicesContainer/>
+     <Routes>
+     <Route path="" element={HomeContainer}/>
+     <Route path="/services" element={<ServicesContainer/>}/>
+     </Routes>
+    </BrowserRouter>
     </div>
   );
 }

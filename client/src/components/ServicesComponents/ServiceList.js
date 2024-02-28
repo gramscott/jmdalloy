604f28d4b80ea8405162fa55ec945dd548a42cde
gramscott <120787031+gramscott@users.noreply.github.com>
2024-02-28 15:14:47 +0000

    structured the services

--- client/src/components/ServicesComponents/ServiceList.js
+++ client/src/components/ServicesComponents/ServiceList.js
@@ -3,12 +3,21 @@ import './ServiceList.css'
 
 const ServiceList = ( {serviceData }) => {
 
+  const halfWayIndex = Math.ceil(serviceData.length / 2);
+  const firstRowServices = serviceData.slice(0, halfWayIndex);
+  const secondRowServices = serviceData.slice(halfWayIndex);
+
   return (
 
     <div className='service-container'>
     <h2>Our Services</h2>
     <div className='service-list'>
-    {serviceData.map(service => (
+    {firstRowServices.map(service => (
+      <ServiceItem key={service.id} service={service} />
+    ))}
+    </div>
+    <div className='service-list'>
+    {secondRowServices.map(service => (
       <ServiceItem key={service.id} service={service} />
     ))}
     </div>

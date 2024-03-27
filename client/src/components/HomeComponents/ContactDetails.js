cf900bb853b3a2c3908360d3aaea36180631fe57
gramscott <120787031+gramscott@users.noreply.github.com>
2024-03-27 15:33:10 +0000

    just the contact form to do

--- client/src/components/HomeComponents/ContactDetails.js
+++ client/src/components/HomeComponents/ContactDetails.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './ContactDetails.css'
-import { Telephone, Instagram, Facebook } from 'react-bootstrap-icons'
+import { Telephone, Instagram, Facebook, GeoAlt } from 'react-bootstrap-icons'
 import { FaEnvelope } from 'react-icons/fa'; 
 const ContactDetails = () => {
   return (
@@ -35,9 +35,10 @@ const ContactDetails = () => {
       <div className='contact-section'>
         <h3>Opening Hours/Location</h3>
         <div className='contact-item'>
-          <a href="https://www.google.com/maps/place/178+Hyndford+Rd,+Lanark+ML11+9BH/@55.6671518,-3.7564874,17z/data=!3m1!4b1!4m6!3m5!1s0x48880ee881db889f:0x4fc60d146f023541!8m2!3d55.6671518!4d-3.7564874!16s%2Fg%2F11crvydy8h?entry=ttu"> 
-            <b>178 Hyndford Road, ML11 9BH, Lanark</b>
-          </a>
+        <b>
+          <GeoAlt size={25} className='icon-spacing' /><a
+           href="https://www.google.com/maps/place/178+Hyndford+Rd,+Lanark+ML11+9BH/@55.6671518,-3.7564874,17z/data=!3m1!4b1!4m6!3m5!1s0x48880ee881db889f:0x4fc60d146f023541!8m2!3d55.6671518!4d-3.7564874!16s%2Fg%2F11crvydy8h?entry=ttu">178 Hyndford Road, Lanark, ML11 9BH</a>
+</b>
         </div>
         <div className='contact-item'>
           <b>Monday-Friday: 08:30 - 17:00</b>

e142e178ae73d0e89bfdc76d2cc0c7f92c1b61a8
gramscott <120787031+gramscott@users.noreply.github.com>
2024-05-28 15:59:00 +0100

    testing

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className="footer">
-    Website Design by<a href="https://graemescott.uk/" target="_blank" rel="noopener noreferrer">Graeme Scott</a>
+    Website Design by<a href="https://graemescott.uk" target="_blank" rel="noopener noreferrer">Graeme Scott</a>
     </div>
   )
 }

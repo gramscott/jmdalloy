c5f1b77fd41dee0cf70f9bddc7884a2c0b173211
gramscott <120787031+gramscott@users.noreply.github.com>
2024-06-21 13:39:09 +0100

    footer is back

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className="footer">
-    Website Design by<a href="mailto:graemesco96@gmail.com" target="_blank" rel="noopener noreferrer">Graeme Scott</a>
+    Website Design by<a href="https://gramscott.github.io/portfolio/" target="_blank" rel="noopener noreferrer">Graeme Scott</a>
     </div>
   )
 }
